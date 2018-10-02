c01edc2c99b9c1706eba0597a53244022b29f6f5
VorapratR <vorapratrattana@gmail.com>
2018-10-02 11:32:58 +0700

    สร้างแอคชั่นให้กด login แล้วไปหน้าใหม่

--- basicapp/src/pages/home/home.ts
+++ basicapp/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  next(){
+    this.navCtrl.push(DetailPage)
   }
 
 }
